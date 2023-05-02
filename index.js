45d6bce64b94ae5c7181ed6465c33df8bb720abd
Syed-Anwarul-Haque-Piash <syed88783@gmail.com>
2023-05-02 23:25:54 +0600

    path done

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ app.get('/', (req, res) => {
 app.get('/recipes', (req, res) => {
     res.send(recipe)
 })
+app.get('/recipes/:id',(req,res)=>{
+  const id=req.params.id;
+  console.log(id)
+  const selectedRecipe=recipe.find(rec=>rec.id==id);
+  res.send(selectedRecipe);
+  
+});
 
 
 app.listen(port, () => {
