62e8f0559eeef3b661b384f1bae4cd2fee9b1cde
Syed-Anwarul-Haque-Piash <syed88783@gmail.com>
2023-05-04 03:25:38 +0600

    process.env file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 var cors = require('cors')
-const port = 5000
+const port = process.env.PORT ||5000
 app.use(cors());
 const recipe=require("./data/data.json")
 
